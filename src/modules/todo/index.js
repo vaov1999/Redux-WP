a8a0d17a275d42a110d53fce2aa22f5e3f4a38fc
vaov1999 <vaov199@gmail.com>
2020-01-28 17:46:12 +0200

    todo saturday 1:41min

--- src/modules/todo/index.js
+++ src/modules/todo/index.js
@@ -1,8 +1,26 @@
 import {store} from "../../store";
+import {addTodo} from "./acitons";
 
 const todoRootNode = document.getElementById('todo-list-root');
 const todoInputNode = document.getElementById('todo-input');
 const todoBtnAddNode = document.getElementById('todo-btn-add');
+
+document.addEventListener('keydown', (event) => {
+    console.log(event);
+    if (event.key === 'Enter') {
+        const title = todoInputNode.value;
+        const addTodoAction = addTodo(title);
+
+        store.dispatch(addTodoAction)
+    }
+});
+
+todoBtnAddNode.addEventListener('click', () => {
+    const title = todoInputNode.value;
+    const addTodoAction = addTodo(title);
+
+    store.dispatch(addTodoAction)
+});
 
 function renderTodoList() {
     const {todoReducer} = store.getState();
