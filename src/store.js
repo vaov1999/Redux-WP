3fa3872175341d7b8630a4f3945443c9471eb77a
vaov1999 <vaov199@gmail.com>
2020-02-26 20:39:41 +0200

    redux react migration on npm

--- src/store.js
+++ src/store.js
@@ -1,10 +1,11 @@
+import { createStore, combineReducers } from 'redux';
 import { todoReducer } from './modules/todo/reducer';
 // eslint-disable-next-line import/no-cycle
 import { counterReducer } from './modules/counter';
 
-const combinedReducers = window.Redux.combineReducers({ todoReducer, counterReducer });
+const combinedReducers = combineReducers({ todoReducer, counterReducer });
 
-export const store = window.Redux.createStore(
+export const store = createStore(
   combinedReducers,
   undefined,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
